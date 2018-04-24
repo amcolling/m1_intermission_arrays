9ea10a1987b998d67d1fc2161b27c2feec85d078
amcolling <amcolling@gmail.com>
2018-04-24 11:31:50 -0600

    finished

--- exit_ticket_1.rb
+++ exit_ticket_1.rb
@@ -1,12 +1,23 @@
 # TO DO:
 # Create an array of six elements - content of your choosing (either integers or strings!)
+
+places = ["Yellowstone", "Arches", "Zion", "Grand Canyon", "Arches", "Yosemite"]
+
 # Access the third element
+places[3]
+
 # Add an element to the end of your array
+places.push("Bryce")
 # Add an element to index 2
+places.insert(2, "Redwoods")
 # Remove the first element from your array
+places.delete_at(0)
 # Remove any duplicates that may exist in your array
+places.uniq("Arches")
 # Create a second array with 3 elements
+activities = ["hiking", "climbing", "camping"]
 # "Sqash" the two arrays together
+places.concat(activities)
 
 # BONUS since we didn't cover today 🦄
 # If your array contains strings, write a sentence using each string. If it contains integers, create a multiplication table for your array.
--- exit_ticket_2.rb
+++ exit_ticket_2.rb
@@ -14,27 +14,56 @@ pet.length
 # YOUR TURN:
 
 hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix"]
+#array of hedgies
 new_hedgies = ["Mooshi", "Monkey"]
+#a new array of new hedgies
 
 hedgies[3]
+#will return the element in the 3rd index= "Ernie"
 hedgies.empty?
+#will return false, as this array is not empty
 hedgies.at[5]
+#
 hedgies.unshift("Bella")
+#will add "Bella" to the beginning of an array
 hedgies.insert(6, "Onyx")
+#inserts "Onyx" into the 6th position in the hedgies array
 hedgies.compact
+
 hedgies.pop
+#removes the last item in the array
 hedgies.fetch(0)
+#returns the element in the zero index
 hedgies.include?("Bagel")
+#will return true or false depending on if the array has "bagel", in this case it will be false
 hedgies.shift
+#will remove the first item in the array, then shift over the remaining elements.
 hedgies.push("Toast")
+#will add "Toast" to the end of an array
 hedgies << "Rocky"
+# adds "Rocky" to the end of the array
 hedgies.count
+#will return number of elements in the Hedgies array = 10. You can also do hedgies.size, or hedgies.length
 # also, what is at least one other way to accomplish this?
 hedgies.uniq!
+#removes duplicates in the array
 # also, how would this be different if we omitted the "!"?
+#returns nil if no duplicate is found, vs. hedgies.uniq
+
 hedgies.concat(new_hedgies)
+#will add the two arrays together ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix","Mooshi", "Monkey"]
 
 # BONUS since we didn't cover today 🦄
 hedgies.each do |hedgie|
   puts hedgie
 end
+Alvin
+Bagel
+Bella
+Ernie
+Kalua
+Margo
+Murphy
+Pino
+Spike
+Trix
